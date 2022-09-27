c94c3edff15764af91b8fcf9f9390cc8d26cabb0
manikanta <manikanta1407@gmail.com>
2022-09-27 22:12:50 +0530

    First Commit-manikanta

--- manikanta.c
+++ manikanta.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 int i=10;
+char str1[10]="manikanta";
+char str2[10];
+strncpy(str2,str1);
+printf("%s",str2);
 printf("%d",i);
 printf("code is running succufully");
 return 0;
